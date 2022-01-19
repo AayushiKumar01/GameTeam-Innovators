e6465c1b0cdf22d1beee01919eef070361f64734
Adam Kucera <83189896+kuceraa1@users.noreply.github.com>
2022-01-19 14:38:27 -0800

    Add SpecialAbility enum class to Models.enum

---
+++ Game/Game/Models/Enum/SpecialAbility.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game.Models
+{
+    public enum SpecialAbility
+    {
+        // Invalid State
+        Unknown = 0,
+
+        // Increase Damage
+        Damage = 1,
+
+        // Increase Shield
+        Shield = 2,
+
+        // Healing over tie
+        Healing = 3,
+
+        // Teleport
+        Teleportation = 4,
+
+        // Increase Speed
+        Speed = 5
+    }
+}
