e01dbb10fb35f7d3b38ce0348d21a0de2b5472f9
akyrimos <akyrimos@uw.edu>
2022-02-06 15:57:57 -0800

    Fixed CharacterModel_GetAttack_Default_Should_Pass() in CharacterModelTests.cs

--- UnitTests/Models/CharacterModelTests.cs
+++ UnitTests/Models/CharacterModelTests.cs
@@ -324,7 +324,7 @@ namespace UnitTests.Models
             // Reset
 
             // Assert
-            Assert.AreEqual(1, result);
+            Assert.AreEqual(3, result);
         }
 
         [Test]
