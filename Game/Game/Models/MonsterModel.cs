33b2fd2f9fd44df02d2cc3435cb63dcb9db60a7e
Aayushi Kumar <akumar4@seattleu.edu>
2022-02-09 14:46:10 -0800

    Updated Default data value in MonsterModel.cs

--- Game/Game/Models/MonsterModel.cs
+++ Game/Game/Models/MonsterModel.cs
@@ -18,12 +18,12 @@ namespace Game.Models
         {
             PlayerType = PlayerTypeEnum.Monster;
             Guid = Id;
-            Name = "Troll";
-            Description = "Angry Troll";
+            Name = "Medusozoan Destroyer";
+            Description = "Watch out for the tentacles";
             Attack = 1;
             Difficulty = DifficultyEnum.Average;
             UniqueItem = null;
-            ImageURI = "troll.png";
+            ImageURI = "monsterdrafts2.png";
             ExperienceTotal = 0;
             ExperienceRemaining = LevelTableHelper.LevelDetailsList[Level + 1].Experience - 1;
 
