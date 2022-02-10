2f3be40d2468549ed02a07b338bce6b867903cdc
Aayushi Kumar <akumar4@seattleu.edu>
2022-02-09 21:03:58 -0800

    Fixed DefaultData_Valid_LoadData_Item_Should_Pass() in DefaultDataTests.cs

--- UnitTests/GameRules/DefaultDataTests.cs
+++ UnitTests/GameRules/DefaultDataTests.cs
@@ -21,7 +21,7 @@ namespace UnitTests.GameRules
             // Reset
 
             // Assert
-            Assert.AreEqual(24, result.Count);
+            Assert.AreEqual(23, result.Count);
         }
 
         [Test]
