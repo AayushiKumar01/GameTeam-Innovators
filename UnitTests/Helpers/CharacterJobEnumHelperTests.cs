72f7c9eb699a66aab221948cf094d13f3f72fa34
Aayushi Kumar <akumar4@seattleu.edu>
2022-02-15 16:53:32 -0800

    Added CharacterJobEnumHelperTests.cs

---
+++ UnitTests/Helpers/CharacterJobEnumHelperTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+
+using NUnit.Framework;
+
+using Game.Models;
+
+namespace UnitTests.Helpers
+{
+    [TestFixture]
+    public class CharacterJobEnumHelperTests
+    {
+        [Test]
+        public void CharacterJobEnumHelper_GetListMessageCharacterJob_Valid_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = CharacterJobEnumHelper.GetListMessageCharacterJob;
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(5, result.Count);
+        }
+
+    }
+}
