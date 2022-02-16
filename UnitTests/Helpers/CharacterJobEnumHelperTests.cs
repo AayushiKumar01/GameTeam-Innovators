c730c398093c4dbb537a0488fb26614fa40ad705
Aayushi Kumar <akumar4@seattleu.edu>
2022-02-15 16:58:39 -0800

    Added coverage for ConvertMessageToEnum in CharacterJobEnumHelperTests.cs for 100% coverage.

--- UnitTests/Helpers/CharacterJobEnumHelperTests.cs
+++ UnitTests/Helpers/CharacterJobEnumHelperTests.cs
@@ -23,5 +23,33 @@ namespace UnitTests.Helpers
             Assert.AreEqual(5, result.Count);
         }
 
+        [Test]
+        public void CharacterJobEnumHelper_ConvertMessageToEnum_Valid_Head_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = CharacterJobEnumHelper.ConvertMessageToEnum("Frigate");
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(CharacterJobEnum.Frigate, result);
+        }
+
+        [Test]
+        public void CharacterJobEnumHelper_ConvertMessageToEnum_InValid_Bogus_Should_Fail()
+        {
+            // Arrange
+
+            // Act
+            var result = CharacterJobEnumHelper.ConvertMessageToEnum("bogus");
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(CharacterJobEnum.Unknown, result);
+        }
+
     }
 }
