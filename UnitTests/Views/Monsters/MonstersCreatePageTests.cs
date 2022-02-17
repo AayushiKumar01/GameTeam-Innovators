516ed83706ca940ab4b75f821e6585a3b164ca45
Adam Kucera <83189896+kuceraa1@users.noreply.github.com>
2022-02-16 16:39:23 -0800

    Add MonsterCreatePage_Save_Clicked_Empty_Description_Shows_Error_Should_Pass to MonstersCreatePageTests.cs

--- UnitTests/Views/Monsters/MonstersCreatePageTests.cs
+++ UnitTests/Views/Monsters/MonstersCreatePageTests.cs
@@ -110,6 +110,21 @@ namespace UnitTests.Views
         }
 
         [Test]
+        public void MonsterCreatePage_Save_Clicked_Empty_Description_Shows_Error_Should_Pass()
+        {
+            // Arrange
+            ((Entry)page.FindByName("MonsterDescription")).Text = "";
+
+            // Act
+            page.Save_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
+        [Test]
         public void MonsterCreatePage_OnBackButtonPressed_Valid_Should_Pass()
         {
             // Arrange
