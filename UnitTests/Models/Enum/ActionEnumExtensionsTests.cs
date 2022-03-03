7f1aa2cea9fbec020cc79d4394db1c2376522286
akyrimos <akyrimos@uw.edu>
2022-03-03 14:31:18 -0800

    Added ActionEnumExtensionsTests_Rest_Default_Should_Pass to ActionEnumExtensionsTests.cs

--- UnitTests/Models/Enum/ActionEnumExtensionsTests.cs
+++ UnitTests/Models/Enum/ActionEnumExtensionsTests.cs
@@ -50,6 +50,20 @@ namespace UnitTests.Models
         }
 
         [Test]
+        public void ActionEnumExtensionsTests_Rest_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = ActionEnum.Rest.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(" Takes a rest ", result);
+        }
+
+        [Test]
         public void ActionEnumExtensionsTests_Attack_Default_Should_Pass()
         {
             // Arrange
