e152fc733b6c455358bd1c0f1c6447acfbf91975
akyrimos <akyrimos@uw.edu>
2022-03-03 23:20:51 -0800

    Update Resource.designer.cs

--- Game/Game.Android/Resources/Resource.designer.cs
+++ Game/Game.Android/Resources/Resource.designer.cs
@@ -16799,115 +16799,118 @@ namespace Game.Droid
 			public const int reactor1 = 2131165372;
 			
 			// aapt resource value: 0x7F0700BD
-			public const int ring1 = 2131165373;
+			public const int reactor2 = 2131165373;
 			
 			// aapt resource value: 0x7F0700BE
-			public const int ring2 = 2131165374;
+			public const int ring1 = 2131165374;
 			
 			// aapt resource value: 0x7F0700BF
-			public const int score = 2131165375;
+			public const int ring2 = 2131165375;
 			
 			// aapt resource value: 0x7F0700C0
-			public const int shield1 = 2131165376;
+			public const int score = 2131165376;
 			
 			// aapt resource value: 0x7F0700C1
-			public const int shield2 = 2131165377;
+			public const int shield1 = 2131165377;
 			
 			// aapt resource value: 0x7F0700C2
-			public const int shield3 = 2131165378;
+			public const int shield2 = 2131165378;
 			
 			// aapt resource value: 0x7F0700C3
-			public const int shield4 = 2131165379;
+			public const int shield3 = 2131165379;
 			
 			// aapt resource value: 0x7F0700C4
-			public const int ship1 = 2131165380;
+			public const int shield4 = 2131165380;
 			
 			// aapt resource value: 0x7F0700C5
-			public const int ship2 = 2131165381;
+			public const int ship1 = 2131165381;
 			
 			// aapt resource value: 0x7F0700C6
-			public const int ship3 = 2131165382;
+			public const int ship2 = 2131165382;
 			
 			// aapt resource value: 0x7F0700C7
-			public const int ship4 = 2131165383;
+			public const int ship3 = 2131165383;
 			
 			// aapt resource value: 0x7F0700C8
-			public const int ship5 = 2131165384;
+			public const int ship4 = 2131165384;
 			
 			// aapt resource value: 0x7F0700C9
-			public const int ship6 = 2131165385;
+			public const int ship5 = 2131165385;
 			
 			// aapt resource value: 0x7F0700CA
-			public const int ship7 = 2131165386;
+			public const int ship6 = 2131165386;
 			
 			// aapt resource value: 0x7F0700CB
-			public const int sword1 = 2131165387;
+			public const int ship7 = 2131165387;
 			
 			// aapt resource value: 0x7F0700CC
-			public const int sword10 = 2131165388;
+			public const int sword1 = 2131165388;
 			
 			// aapt resource value: 0x7F0700CD
-			public const int sword11 = 2131165389;
+			public const int sword10 = 2131165389;
 			
 			// aapt resource value: 0x7F0700CE
-			public const int sword2 = 2131165390;
+			public const int sword11 = 2131165390;
 			
 			// aapt resource value: 0x7F0700CF
-			public const int sword3 = 2131165391;
+			public const int sword2 = 2131165391;
 			
 			// aapt resource value: 0x7F0700D0
-			public const int sword4 = 2131165392;
+			public const int sword3 = 2131165392;
 			
 			// aapt resource value: 0x7F0700D1
-			public const int sword5 = 2131165393;
+			public const int sword4 = 2131165393;
 			
 			// aapt resource value: 0x7F0700D2
-			public const int sword6 = 2131165394;
+			public const int sword5 = 2131165394;
 			
 			// aapt resource value: 0x7F0700D3
-			public const int sword7 = 2131165395;
+			public const int sword6 = 2131165395;
 			
 			// aapt resource value: 0x7F0700D4
-			public const int sword8 = 2131165396;
+			public const int sword7 = 2131165396;
 			
 			// aapt resource value: 0x7F0700D5
-			public const int sword9 = 2131165397;
+			public const int sword8 = 2131165397;
 			
 			// aapt resource value: 0x7F0700D6
-			public const int test_custom_background = 2131165398;
+			public const int sword9 = 2131165398;
 			
 			// aapt resource value: 0x7F0700D7
-			public const int thruster1 = 2131165399;
+			public const int test_custom_background = 2131165399;
 			
 			// aapt resource value: 0x7F0700D8
-			public const int tooltip_frame_dark = 2131165400;
+			public const int thruster1 = 2131165400;
 			
 			// aapt resource value: 0x7F0700D9
-			public const int tooltip_frame_light = 2131165401;
+			public const int tooltip_frame_dark = 2131165401;
 			
 			// aapt resource value: 0x7F0700DA
-			public const int troll = 2131165402;
+			public const int tooltip_frame_light = 2131165402;
 			
 			// aapt resource value: 0x7F0700DB
-			public const int troll1 = 2131165403;
+			public const int troll = 2131165403;
 			
 			// aapt resource value: 0x7F0700DC
-			public const int troll2 = 2131165404;
+			public const int troll1 = 2131165404;
 			
 			// aapt resource value: 0x7F0700DD
-			public const int troll3 = 2131165405;
+			public const int troll2 = 2131165405;
 			
 			// aapt resource value: 0x7F0700DE
-			public const int troll4 = 2131165406;
+			public const int troll3 = 2131165406;
 			
 			// aapt resource value: 0x7F0700DF
-			public const int troll5 = 2131165407;
+			public const int troll4 = 2131165407;
 			
 			// aapt resource value: 0x7F0700E0
-			public const int troll6 = 2131165408;
+			public const int troll5 = 2131165408;
 			
 			// aapt resource value: 0x7F0700E1
-			public const int village = 2131165409;
+			public const int troll6 = 2131165409;
+			
+			// aapt resource value: 0x7F0700E2
+			public const int village = 2131165410;
 			
 			static Drawable()
 			{
