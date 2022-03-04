fb91f58173470eebe1ff5e7f0800adcf395c411a
Aayushi Kumar <akumar4@seattleu.edu>
2022-03-03 19:31:23 -0800

    removing reference to AudioPlayer from AutoBattlePage.cs

--- Game/Game/Views/Battle/AutoBattlePage.xaml.cs
+++ Game/Game/Views/Battle/AutoBattlePage.xaml.cs
@@ -19,8 +19,6 @@ namespace Game.Views
         // Hold the Engine, so it can be swapped out for unit testing
         public IAutoBattleInterface AutoBattle = BattleEngineViewModel.Instance.AutoBattleEngine;
 
-        private AudioHelper audio;
-
         /// <summary>
         /// Constructor
         /// </summary>
