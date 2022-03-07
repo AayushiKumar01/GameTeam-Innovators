69645636a72a3f8ee297eff711bb07510797a94b
Aayushi Kumar <akumar4@seattleu.edu>
2022-03-07 12:16:01 -0800

    Added BattlePage_RestButton_Clicked_Default_Should_Pass() UT in BattlePageTests.cs

--- UnitTests/Views/Battle/BattlePageTests.cs
+++ UnitTests/Views/Battle/BattlePageTests.cs
@@ -94,6 +94,19 @@ namespace UnitTests.Views
         }
 
         [Test]
+        public void BattlePage_RestButton_Clicked_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            page.RestButton_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+        [Test]
         public void BattlePage_FinishButton_Clicked_Default_Should_Pass()
         {
             // Arrange
