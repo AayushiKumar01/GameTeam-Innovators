38269f82f4c491dfe06528032bb4e3bfab9c2d0d
Aayushi Kumar <akumar4@seattleu.edu>
2022-03-13 01:18:41 -0800

    Added test cases for left and right arrow buttons in MonsterUpdatePageTests.cs

--- UnitTests/Views/Monsters/MonstersUpdatePageTests.cs
+++ UnitTests/Views/Monsters/MonstersUpdatePageTests.cs
@@ -204,5 +204,34 @@ namespace UnitTests.Views
             Assert.IsTrue(true); // Got to here, so it happened...
         }
 
+        [Test]
+        public void MonsterUpdatePage_LeftArrow_Clicked_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            page.LeftArrow_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
+        [Test]
+        public void MonsterUpdatePage_RightArrow_Clicked_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            page.RightArrow_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
+
     }
 }
