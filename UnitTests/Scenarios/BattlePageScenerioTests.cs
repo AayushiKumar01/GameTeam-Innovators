ad39ada989841913d2d3e5728e10456a5392207b
Adam Kucera <83189896+kuceraa1@users.noreply.github.com>
2022-03-14 23:00:06 -0700

    Add BattlePage_SetSelectedMonster_Scenerio_Should_Pass to BattlePageScenerioTests.cs

--- UnitTests/Scenarios/BattlePageScenerioTests.cs
+++ UnitTests/Scenarios/BattlePageScenerioTests.cs
@@ -1,10 +1,14 @@
 ﻿
+using System.Linq;
 using Xamarin.Forms;
 using Xamarin.Forms.Mocks;
 
 using NUnit.Framework;
 using Game.Views;
 using Game;
+using Game.Engine.EngineModels;
+using Game.Models;
+using Game.ViewModels;
 
 namespace Scenario
 {
@@ -44,5 +48,57 @@ namespace Scenario
             // Assert
             Assert.IsNotNull(result);
         }
+
+        [Test]
+        public void BattlePage_SetSelectedMonster_Scenerio_Should_Pass()
+        {
+            
+            // Arrange
+            page = new BattlePage();
+            BattleEngineViewModel.Instance.Engine.EngineSettings.EnableMapClick = true;
+            EngineSettingsModel engineSettings = BattleEngineViewModel.Instance.Engine.EngineSettings;
+            PlayerInfoModel character = new PlayerInfoModel(new CharacterModel());
+            character.PlayerType = PlayerTypeEnum.Character;
+            PlayerInfoModel monster = new PlayerInfoModel(new MonsterModel());
+            monster.PlayerType = PlayerTypeEnum.Monster;
+            
+            MapModelLocation characterLocation = new MapModelLocation();
+            characterLocation.Player = character;
+            characterLocation.Column = 1;
+            characterLocation.Row = 1;
+            
+            MapModelLocation monsterLocation = new MapModelLocation();
+            monsterLocation.Player = monster;
+            monsterLocation.Column = 2;
+            monsterLocation.Row = 2;
+            
+            //Switch them since NextAttack with switch them again
+            engineSettings.CurrentAttacker = monster;
+            engineSettings.CurrentDefender = character;
+            
+            engineSettings.CharacterList.Clear();
+            engineSettings.MonsterList.Clear();
+            engineSettings.PlayerList.Clear();
+            engineSettings.CharacterList.Add(character);
+            engineSettings.MonsterList.Add(monster);
+            engineSettings.PlayerList.Add(character);
+            engineSettings.PlayerList.Add(monster);
+            engineSettings.MapModel.MapGridLocation[1, 1] = characterLocation;
+            engineSettings.MapModel.MapGridLocation[2, 2] = monsterLocation;
+            page.NextAttackExample();
+            
+            // Act
+            var result = page.SetSelectedMonster(monsterLocation);
+
+            // Reset
+            engineSettings.CharacterList.Clear();
+            engineSettings.MonsterList.Clear();
+            engineSettings.PlayerList.Clear();
+            
+            // Assert
+            Assert.AreEqual(RoundEnum.NextTurn, result); // Got to here, so it happened...
+        }
+        
+        
     }
 }
